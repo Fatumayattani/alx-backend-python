3f41a9f391aaaf9eb1c55e5f4cf85b63b198b10d
Fatumayattani <fyattani@gmail.com>
2023-07-31 09:10:50 +0000

    save changes

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Unittest module for testing the GithubOrgClient class """
+""" Unittest module """
 
 import unittest
 from unittest.mock import patch, Mock, PropertyMock
@@ -16,8 +16,8 @@ class TestGithubOrgClient(unittest.TestCase):
         ("abc"),
     ])
     @patch('client.get_json')
-    def test_org_method_returns_correct_output(self, org_name, mock_json):
-        """ Test if the org method returns the correct output """
+    def test_org(self, org_name, mock_json):
+        """ Test method returns correct output """
         endpoint = 'https://api.github.com/orgs/{}'.format(org_name)
         spec = GithubOrgClient(data)
         spec.org()
@@ -26,8 +26,9 @@ class TestGithubOrgClient(unittest.TestCase):
     @parameterized.expand([
         ("random-url", {'repos_url': 'http://some_url.com'})
     ])
-    def test_public_repos_url_returns_correct_output(self, name, result):
-        """ Test if the public_repos_url property returns the correct output """
-        with patch('client.GithubOrgClient.org', PropertyMock(return_value=result)):
+    def test_public_repos_url(self, name, result):
+        """ Test method returns correct output """
+        with patch('client.GithubOrgClient.org',
+                   PropertyMock(return_value=result)):
             response = GithubOrgClient(name)._public_repos_url
             self.assertEqual(response, result.get('repos_url'))
